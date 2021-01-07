6a4b8651d1159e31a6baac019a1e7dff8798875c
Sára Koncz <koncz.sara@appsint.hu>
2021-01-07 19:48:52 +0100

    Updates

---
+++ precache-manifest.5b597a75e4541bc075d8b0c9736e28b8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c183db3cdc1bd3f4acc66338445096",
+    "url": "/poultry-tech/index.html"
+  },
+  {
+    "revision": "57f63f1ccdc57c37a7e4",
+    "url": "/poultry-tech/static/css/2.92d5fef7.chunk.css"
+  },
+  {
+    "revision": "261aea08ab68cb27610b",
+    "url": "/poultry-tech/static/css/main.11b88102.chunk.css"
+  },
+  {
+    "revision": "57f63f1ccdc57c37a7e4",
+    "url": "/poultry-tech/static/js/2.0f8c2ef7.chunk.js"
+  },
+  {
+    "revision": "261aea08ab68cb27610b",
+    "url": "/poultry-tech/static/js/main.cddf5c76.chunk.js"
+  },
+  {
+    "revision": "73198a91136173152f53",
+    "url": "/poultry-tech/static/js/runtime~main.4f558c77.js"
+  },
+  {
+    "revision": "4fe6f9caff8b287170d51d3d71d5e5c6",
+    "url": "/poultry-tech/static/media/lg.4fe6f9ca.ttf"
+  },
+  {
+    "revision": "5fd4c338c1a1b1eeeb2c7b0a0967773d",
+    "url": "/poultry-tech/static/media/lg.5fd4c338.woff"
+  },
+  {
+    "revision": "c066c5448562b3ccaefb6408ce4b4ae1",
+    "url": "/poultry-tech/static/media/lg.c066c544.svg"
+  },
+  {
+    "revision": "ecff11700aad0000cf3503f537d1df17",
+    "url": "/poultry-tech/static/media/lg.ecff1170.eot"
+  }
+]);
